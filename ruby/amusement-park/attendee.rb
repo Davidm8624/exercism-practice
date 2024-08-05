7e9f5c0e7d1ff2d9a52869a80f4330158319299e
davidm8624 <davidm8624@gmail.com>
2024-08-05 14:37:22 -0700

    starting log

--- ruby/amusement-park/attendee.rb
+++ ruby/amusement-park/attendee.rb
@@ -4,33 +4,29 @@ class Attendee
   end
 
   def height
-    puts "you are #{@height} tall"
     return @height #might aswell be explicit
   end
 
   def pass_id
-    puts "your id is #{@id}"
     return @id
   end
 
   def issue_pass!(pass_id)
-    puts "your id is now #{@id}"
     @id = pass_id
   end
 
   def revoke_pass!
-    puts "your id has been revoked"
     @id = nil
   end
 end
 
-#tests
+# Examples
 if __FILE__ == $PROGRAM_NAME
   person = Attendee.new(70)
-  person.height
-  person.pass_id
-  person.issue_pass!(69)
-  person.pass_id
-  person.revoke_pass!
-  person.pass_id
+  puts "You are #{person.height} tall."
+  puts "Your pass ID is: #{person.pass_id}."
+  puts "We will issue you pass #{person.issue_pass!(69)}."
+  puts "Your new pass ID is: #{person.pass_id}."
+  puts "We have to revoke your pass: #{person.revoke_pass!}."
+  puts "Your new pass id, having been revoked is: #{person.pass_id}."
 end
--- ruby/amusement-park/test.rb
+++ ruby/amusement-park/test.rb
@@ -1,9 +1,9 @@
 require_relative 'attendee'
 
-person = Attendee.new(70)
-person.height
-person.pass_id
-person.issue_pass!(69)
-person.pass_id
-person.revoke_pass!
-person.pass_id
+  person = Attendee.new(70)
+  puts "You are #{person.height} tall."
+  puts "Your pass ID is: #{person.pass_id}."
+  puts "We will issue you pass #{person.issue_pass!(69)}."
+  puts "Your new pass ID is: #{person.pass_id}."
+  puts "We have to revoke your pass: #{person.revoke_pass!}."
+  puts "Your new pass id, having been revoked is: #{person.pass_id}."
