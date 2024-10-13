19f35af28d7e106cfa32e9ba8ff1d1aac23e8767
davidm8624 <davidm8624@gmail.com>
2024-10-12 17:43:31 -0700

    blackjack bot

--- ruby/blackjack/blackjack.rb
+++ ruby/blackjack/blackjack.rb
@@ -1,13 +1,61 @@
 module Blackjack
   def self.parse_card(card)
-    raise "Please implement the Blackjack.parse_card method"
+    case card
+    when 'ace'
+      11
+    when 'two'
+      2
+    when 'three'
+      3
+    when 'four'
+      4
+    when 'five'
+      5
+    when 'six'
+      6
+    when 'seven'
+      7
+    when 'eight'
+      8
+    when 'nine'
+      9
+    when 'ten'
+      10
+    when 'jack'
+      10
+    when 'queen'
+      10
+    when 'king'
+      10
+    else
+      0
+    end
   end
 
   def self.card_range(card1, card2)
-    raise "Please implement the Blackjack.card_range method"
+    hand = parse_card(card1) + parse_card(card2)
+    case hand
+    when (4..11) then 'low'
+    when (12..16) then 'mid'
+    when (17..20) then 'high'
+    when 21 then 'blackjack'
+    end
   end
 
   def self.first_turn(card1, card2, dealer_card)
-    raise "Please implement the Blackjack.first_turn method"
+    hand = card_range(card1, card2)
+    dealer_value = parse_card(dealer_card)
+    case
+    when [card1, card2] == ['ace', 'ace']
+      'P'
+    when hand == 'blackjack'
+      dealer_value < 10 ? 'W' : 'S'
+    when hand == 'high'
+      'S'
+    when hand == 'mid'
+      dealer_value >= 7 ? 'H' : 'S'
+    when hand == 'low'
+      'H'
+    end
   end
 end
