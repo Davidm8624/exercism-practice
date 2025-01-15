089ac799be435da895629ecd40093ee75400c00e
davidm8624 <davidm8624@gmail.com>
2025-01-14 17:32:58 -0700

    restart

--- ruby/amusement-park/test.rb
+++
@@ -1,9 +0,0 @@
-require_relative 'attendee'
-
-person = Attendee.new(70)
-puts "You are #{person.height} tall."
-puts "Your pass ID is: #{person.pass_id}."
-puts "We will issue you pass #{person.issue_pass!(69)}."
-puts "Your new pass ID is: #{person.pass_id}."
-puts "We have to revoke your pass: #{person.revoke_pass!}."
-puts "Your new pass id, having been revoked is: #{person.pass_id}."
