acceaa4587406bbdf04e761dbeec7894c2889dfb
davidm8624 <davidm8624@gmail.com>
2025-02-07 18:04:49 -0700

    2/7

--- ruby/assembly-line/assembly_line.rb
+++ ruby/assembly-line/assembly_line.rb
@@ -1,13 +1,27 @@
 class AssemblyLine
   def initialize(speed)
-    @speed = speed
+    @speed = speed.to_f
+  end
+  
+  def chances(rate)
+    case rate
+      when 1..4
+        return (1.0 * rate)
+      when  5..8
+        return (0.9 * rate)
+      when 9
+        return (0.8 * rate)
+      when 10
+        return (0.77 * rate)
+    end
   end
 
   def production_rate_per_hour
-    raise 'Please implement the AssemblyLine#production_rate_per_hour method'
+    return chances(@speed)*221
   end
 
   def working_items_per_minute
-    raise 'Please implement the AssemblyLine#working_items_per_minute method'
+    (production_rate_per_hour / 60).to_i
   end
 end
+
