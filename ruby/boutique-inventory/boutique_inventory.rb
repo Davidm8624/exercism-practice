834bb001fcfff76ff2b3cb9e5ce7a2f5669f79bc
davidm8624 <davidm8624@gmail.com>
2025-02-20 18:48:06 -0700

    rubocop

--- ruby/boutique-inventory/boutique_inventory.rb
+++ ruby/boutique-inventory/boutique_inventory.rb
@@ -4,15 +4,15 @@ class BoutiqueInventory
   end
 
   def item_names
-    return @items.map {|item| item[:name]}.sort
+    return @items.map { |item| item[:name] }.sort
   end
 
   def cheap
-    return @items.select {|item| item[:price] < 30}
+    return @items.select { |item| item[:price] < 30 }
   end
 
   def out_of_stock
-    return @items.select {|item| item[:quantity_by_size].empty?}
+    return @items.select { |item| item[:quantity_by_size].empty? }
   end
 
   def stock_for_item(name)
@@ -20,9 +20,10 @@ class BoutiqueInventory
   end
 
   def total_stock
-    @items.map {|item| item[:quantity_by_size].values.sum}.sum
+    @items.map { |item| item[:quantity_by_size].values.sum }.sum
   end
 
   private
+
   attr_reader :items
 end
