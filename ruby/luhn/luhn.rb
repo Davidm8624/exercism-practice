dfeeaa325f7bca7b6d1d8223a0327ebfde830e9a
davidm8624 <davidm8624@gmail.com>
2025-03-15 18:02:11 -0700

    3/15

--- ruby/luhn/luhn.rb
+++ ruby/luhn/luhn.rb
@@ -1,7 +1,35 @@
-=begin
-Write your code for the 'Luhn' exercise in this file. Make the tests in
-`luhn_test.rb` pass.
+class Luhn
+  def self.valid?(value)
+    clean_value = value.gsub(' ', '')
 
-To get started with TDD, see the `README.md` file in your
-`ruby/luhn` directory.
-=end
+    # Ensure input contains only digits and is not a single digit
+    return false unless clean_value.match?(/\A\d+\z/) && clean_value.length > 1
+
+    digits = clean_value.chars.map(&:to_i)
+
+    even_values = []
+    odd_values = []
+
+    digits.reverse.each_with_index do |val, index|
+      if index.even?
+        even_values << val
+      else
+        odd_values << val
+      end
+    end
+
+    result = { even: even_values, odd: odd_values }
+    odd_index_items = result[:odd]
+    doubled_odd_items = odd_index_items.map { |item| item * 2 }
+    result[:odd] = doubled_odd_items
+
+    result.keys.each do |key|
+      result[key] = result[key].map { |item| item > 9 ? item - 9 : item }
+    end
+
+    sum = result.values.flatten.reduce(:+)
+
+    # Only check that sum is a multiple of 10
+    sum % 10 == 0
+  end
+end
